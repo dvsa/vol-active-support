515ca1e96370fd5b3781f7a1146232d50acb369b
Wonderson Chideya <wondersonchideya@gmail.com>
2023-05-03 15:23:41 +0100

    Revert "removed browser util from package"

---
+++ src/main/java/activesupport/driver/Parallel/ChromeSetUp.java
@@ -0,0 +1,42 @@
+package activesupport.driver.Parallel;
+
+import activesupport.proxy.ProxyConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static activesupport.driver.Browser.*;
+
+public class ChromeSetUp {
+
+    private ChromeOptions chromeOptions = new ChromeOptions();
+
+    public ChromeOptions getChromeOptions() {
+        return chromeOptions;
+    }
+
+    public void setChromeOptions(ChromeOptions chromeOptions) {
+        this.chromeOptions = chromeOptions;
+    }
+
+    public static WebDriver driver;
+    public WebDriver driver() throws MalformedURLException {
+        WebDriverManager.chromedriver().setup();
+        chromeOptions.addArguments(ProxyConfig.ignoreCertErrors());
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        if (getBrowserVersion() == null) {
+            driver = new ChromeDriver(getChromeOptions());
+        } else {
+            chromeOptions.setCapability("proxy",ProxyConfig.dvsaProxy());
+            chromeOptions.setPlatformName(getPlatform());
+            chromeOptions.setCapability("browser_version", getBrowserVersion());
+            driver = new RemoteWebDriver(new URL(hubURL()), getChromeOptions());
+        }
+        return driver;
+    }
+}
