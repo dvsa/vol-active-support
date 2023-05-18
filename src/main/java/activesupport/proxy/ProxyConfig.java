4d0b40f06db36337de826da0efaaaeb73a3c6e00
1dson <wonderson.chideya@dvsa.gov.uk>
2023-05-18 10:35:03 +0100

    proxy class tweak

--- src/main/java/activesupport/proxy/ProxyConfig.java
+++ src/main/java/activesupport/proxy/ProxyConfig.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.Proxy;
 public class ProxyConfig {
     public static Proxy dvsaProxy() {
         Proxy proxy = new Proxy();
-        proxy.setSslProxy(System.getProperty("httpProxy"));
+        proxy.setHttpProxy(System.getProperty("httpProxy"));
+        proxy.setSslProxy(System.getProperty("httpsProxy"));
+        proxy.setNoProxy(System.getProperty("noProxy"));
         return proxy;
     }
 }
