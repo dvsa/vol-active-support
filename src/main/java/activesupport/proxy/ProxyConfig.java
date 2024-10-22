218b844d92419fb7f34a28c60dae8a9746601f20
Stefan Ranoszek <stefan48500@gmail.com>
2024-10-22 13:38:26 +0100

    chore: remove proxy arg

--- src/main/java/activesupport/proxy/ProxyConfig.java
+++ src/main/java/activesupport/proxy/ProxyConfig.java
@@ -1,17 +1,13 @@
 package activesupport.proxy;
 
-import activesupport.aws.s3.SecretsManager;
 import org.openqa.selenium.Proxy;
 
 public class ProxyConfig {
     public static Proxy dvsaProxy() {
         Proxy proxy = new Proxy();
-        proxy.setHttpProxy(SecretsManager.getSecretValue("httpProxy"));
-        proxy.setSslProxy(SecretsManager.getSecretValue("httpsProxy"));
+        proxy.setHttpProxy(System.getProperty("httpProxy"));
+        proxy.setSslProxy(System.getProperty("httpsProxy"));
         proxy.setNoProxy(System.getProperty("noProxy"));
-        System.out.println(proxy.getHttpProxy());
-        System.out.println(proxy.getSslProxy());
-        System.out.println(proxy.getNoProxy());
         return proxy;
     }
 }
